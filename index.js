fb3367a76559a59f65ba921f46fff83bd82453ce
muurucosmas <cosmasmuuuru988@gmail.com>
2026-04-17 01:08:08 +0300

    used modules and parameterzs

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+require("datejs");
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (let arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 
 module.exports = {
